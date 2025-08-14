3da5a2aaf6ee760b1935a8612be2e1fea6fa8280
D0mazo <dominykas.simanavicius@proton.me>
2025-08-14 15:45:57 +0300

    working but not in full

--- components/translator.js
+++ components/translator.js
@@ -1,10 +1,76 @@
+'use strict';
+
 const americanOnly = require('./american-only.js');
 const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const americanToBritishTitles = require("./american-to-british-titles.js");
+const britishOnly = require('./british-only.js');
 
 class Translator {
+  constructor() {
+    this.highlight = (text) => `<span class="highlight">${text}</span>`;
+  }
 
+  translate(text, locale) {
+    if (!text) return { error: 'No text to translate' };
+    if (!locale || (locale !== 'american-to-british' && locale !== 'british-to-american')) {
+      return { error: 'Invalid value for locale field' };
+    }
+
+    let translation = text;
+
+    if (locale === 'american-to-british') {
+      // American-only words
+      Object.keys(americanOnly).forEach(word => {
+        const regex = new RegExp(`\\b${word}\\b`, 'gi');
+        translation = translation.replace(regex, (match) => this.highlight(americanOnly[word]));
+      });
+
+      // Spelling
+      Object.keys(americanToBritishSpelling).forEach(word => {
+        const regex = new RegExp(`\\b${word}\\b`, 'gi');
+        translation = translation.replace(regex, (match) => this.highlight(americanToBritishSpelling[word]));
+      });
+
+      // Titles
+      Object.keys(americanToBritishTitles).forEach(title => {
+        const regex = new RegExp(`\\b${title}\\b`, 'gi');
+        translation = translation.replace(regex, (match) => this.highlight(americanToBritishTitles[title]));
+      });
+
+      // Time formatting
+      translation = translation.replace(/(\d{1,2}):(\d{2})/g, (_, h, m) => this.highlight(`${h}.${m}`));
+
+    } else if (locale === 'british-to-american') {
+      // British-only words
+      Object.keys(britishOnly).forEach(word => {
+        const regex = new RegExp(`\\b${word}\\b`, 'gi');
+        translation = translation.replace(regex, (match) => this.highlight(britishOnly[word]));
+      });
+
+      // Spelling (reverse)
+      Object.keys(americanToBritishSpelling).forEach(word => {
+        const britishWord = americanToBritishSpelling[word];
+        const regex = new RegExp(`\\b${britishWord}\\b`, 'gi');
+        translation = translation.replace(regex, (match) => this.highlight(word));
+      });
+
+      // Titles (reverse)
+      Object.keys(americanToBritishTitles).forEach(title => {
+        const britishTitle = americanToBritishTitles[title];
+        const regex = new RegExp(`\\b${britishTitle}\\b`, 'gi');
+        translation = translation.replace(regex, (match) => this.highlight(title));
+      });
+
+      // Time formatting
+      translation = translation.replace(/(\d{1,2})\.(\d{2})/g, (_, h, m) => this.highlight(`${h}:${m}`));
+    }
+
+    if (translation === text) {
+      return { text, translation: 'Everything looks good to me!' };
+    }
+
+    return { text, translation };
+  }
 }
 
 module.exports = Translator;
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,8 +1,69 @@
 const chai = require('chai');
 const assert = chai.assert;
-
 const Translator = require('../components/translator.js');
 
 suite('Unit Tests', () => {
+  const translator = new Translator();
 
+  test('Translate Mangoes are my favorite fruit. to British English', () => {
+    const result = translator.translate('Mangoes are my favorite fruit.', 'american-to-british');
+    assert.include(result.translation, '<span class="highlight">favourite</span>');
+  });
+
+  test('Translate I ate yogurt for breakfast. to British English', () => {
+    const result = translator.translate('I ate yogurt for breakfast.', 'american-to-british');
+    assert.include(result.translation, '<span class="highlight">yoghurt</span>');
+  });
+
+  test('Translate We had a party at my friend\'s condo. to British English', () => {
+    const result = translator.translate('We had a party at my friend\'s condo.', 'american-to-british');
+    assert.include(result.translation, '<span class="highlight">flat</span>');
+  });
+
+  test('Translate Can you toss this in the trashcan for me? to British English', () => {
+    const result = translator.translate('Can you toss this in the trashcan for me?', 'american-to-british');
+    assert.include(result.translation, '<span class="highlight">bin</span>');
+  });
+
+  test('Translate The parking lot was full. to British English', () => {
+    const result = translator.translate('The parking lot was full.', 'american-to-british');
+    assert.include(result.translation, '<span class="highlight">car park</span>');
+  });
+
+  test('Translate Like a high tech Rube Goldberg machine. to British English', () => {
+    const result = translator.translate('Like a high tech Rube Goldberg machine.', 'american-to-british');
+    assert.include(result.translation, '<span class="highlight">Heath Robinson</span>');
+  });
+
+  test('Translate To play hooky means to skip class or work. to British English', () => {
+    const result = translator.translate('To play hooky means to skip class or work.', 'american-to-british');
+    assert.include(result.translation, '<span class="highlight">bunk off</span>');
+  });
+
+  test('Translate No Mr. Bond, I expect you to die. to British English', () => {
+    const result = translator.translate('No Mr. Bond, I expect you to die.', 'american-to-british');
+    assert.include(result.translation, '<span class="highlight">Mr</span>');
+  });
+
+  test('Translate Dr. Grosh will see you now. to British English', () => {
+    const result = translator.translate('Dr. Grosh will see you now.', 'american-to-british');
+    assert.include(result.translation, '<span class="highlight">Dr</span>');
+  });
+
+  test('Translate Lunch is at 12:15 today. to British English', () => {
+    const result = translator.translate('Lunch is at 12:15 today.', 'american-to-british');
+    assert.include(result.translation, '<span class="highlight">12.15</span>');
+  });
+
+  test('Translate We watched the footie match for a while. to American English', () => {
+    const result = translator.translate('We watched the footie match for a while.', 'british-to-american');
+    assert.include(result.translation, '<span class="highlight">soccer</span>');
+  });
+
+  test('Translate Paracetamol takes up to an hour to work. to American English', () => {
+    const result = translator.translate('Paracetamol takes up to an hour to work.', 'british-to-american');
+    assert.include(result.translation, '<span class="highlight">acetaminophen</span>');
+  });
+
+  // ...add more tests for remaining translations and highlights
 });
